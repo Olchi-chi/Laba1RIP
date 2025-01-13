9dbfdbdb1703ddbee56ac5abf0f27f82a6319685
Ольга <you@example.com>
2025-01-13 14:46:23 +0700

    adding crud

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
- 
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
 app = FastAPI()
- 
-@app.get("/")
-def read_root():
-    html_content = "<h2>Hello METANIT.COM!</h2>"
-    return HTMLResponse(content=html_content)
+
+class Project(BaseModel):
+    id: int
+    name: str
+    description: str = None
+    date: str
+    podrazdelenie: str
+
+@app.post("/projects/")
+async def create_project(project: Project):
+    return project
+
+@app.get("/projects/{project_id}")
+async def read_project(project_id: int):
+    return {"project_id": project_id}
+
+@app.put("/projects/{project_id}")
+async def update_project(project_id: int, project: Project):
+    return {"project_id": project_id, "project": project}
+
+@app.delete("/projects/{project_id}")
+async def delete_project(project_id: int):
+    return {"message": "Project deleted", "project_id": project_id}
